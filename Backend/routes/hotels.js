72f2962266b2970df32dd7ec6f65d41071484fc3
Sourabh Sharma <sourabhsharma4779@gmail.com>
2025-08-25 20:17:52 +0530

    Initial commit with placeholder env

---
+++ Backend/routes/hotels.js
@@ -0,0 +1,29 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+} from "../controllers/hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getHotels );
+
+export default router
